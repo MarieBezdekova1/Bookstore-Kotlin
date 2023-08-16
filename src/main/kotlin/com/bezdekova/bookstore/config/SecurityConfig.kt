1e05149fb10a87b0afb2a991f82d417798c9827a
bezdekovam <marie.zachovalova@gmail.com>
2023-08-16 09:49:29 +0200

    přidána jednoduchá autentizace

---
+++ src/main/kotlin/com/bezdekova/bookstore/config/SecurityConfig.kt
@@ -0,0 +1,49 @@
+package com.bezdekova.bookstore.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+                .authorizeHttpRequests { requests ->
+                    requests
+                            .requestMatchers(HttpMethod.POST).hasRole("USER1")
+                            .requestMatchers("/api/v1/authors").permitAll()
+                            .anyRequest().authenticated()
+                }
+                .csrf { it.disable() }
+                .httpBasic(Customizer.withDefaults())
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun testOnlyUsers(passwordEncoder: PasswordEncoder): UserDetailsService {
+        val user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build()
+        return InMemoryUserDetailsManager(user, user)
+    }
+}
